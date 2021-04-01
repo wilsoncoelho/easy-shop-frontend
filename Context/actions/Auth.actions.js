4ef600a7a976d6025013766e59002b53b5b0a4b1
Wilson Coelho <wilsonsince1990@gmail.com>
2021-04-01 11:15:42 +0530

    updated

--- Context/actions/Auth.actions.js
+++ Context/actions/Auth.actions.js
@@ -3,67 +3,61 @@ import AsyncStorage from '@react-native-community/async-storage'
 import baseURL from '../../assets/common/baseUrl'
 import Toast from 'react-native-toast-message'
 
-export const SET_CURRENT_USER =  "SET_CURRENT_USER"
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
 export const loginUser = (user, dispatch) => {
     fetch(`${baseURL}users/login`, {
-        method:'POST',
-        body:JSON.stringify(user),
-        headers:{
-            Accept:'application/json',
-            "Content-Type":'application/json',
-        }
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
     })
-    .then((res)=>
-        res.json()
-    )
-    .then((data)=> {
-        if(data){
+    .then((res) => res.json())
+    .then((data) => {
+        if (data) {
             const token = data.token;
             AsyncStorage.setItem("jwt", token)
             const decoded = jwt_decode(token)
-            dispatch(setCurrentUser(decoded, user))//TODO
+            dispatch(setCurrentUser(decoded, user))
         } else {
-            logoutUser(dispatch)
+           logoutUser(dispatch)
         }
     })
-    .catch((e)=>{
+    .catch((err) => {
         Toast.show({
-            topOffset:60,
-            type:'error',
-            text1:'Please provide correct credentials',
-            text2:''
+            topOffset: 60,
+            type: "error",
+            text1: "Please provide correct credentials",
+            text2: ""
         });
         logoutUser(dispatch)
-    })
-}
+    });
+};
 
-export const getUserProfile=(id)=> {
+export const getUserProfile = (id) => {
     fetch(`${baseURL}users/${id}`, {
-        method:'GET',
-        body:JSON.stringify(user) ,
-        headers:{
-            Accept:'application/json',
-            "Content-Type":'application/json',
-        }
+        method: "GET",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
     })
-    .then((res)=>{
-        res.json()
-    })
-    .then((data)=> {
-        console.log(data)
-    })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
 
-export const logoutUser=(dispatch)=> {
-    AsyncStorage.removeItem('jwt')
+export const logoutUser = (dispatch) => {
+    AsyncStorage.removeItem("jwt");
     dispatch(setCurrentUser({}))
 }
 
 export const setCurrentUser = (decoded, user) => {
     return {
-        type:SET_CURRENT_USER,
-        payload:decoded,
-        userProfile:user
+        type: SET_CURRENT_USER,
+        payload: decoded,
+        userProfile: user
     }
 }
--- Context/reducers/Auth.reducer.js
+++ Context/reducers/Auth.reducer.js
@@ -1,17 +1,15 @@
 import  {SET_CURRENT_USER} from '../actions/Auth.actions'
 import isEmpty from '../../assets/common/isEmpty'
-
 export default function (state, action) {
-    switch(action.type){
-        case SET_CURRENT_USER:
-        return{
+    switch (action.type) {
+        case SET_CURRENT_USER: 
+        return {
             ...state,
-            isAuthenticated:!isEmpty(action.payload),
-            user:action.payload,
-            userProfile:action.userProfile
-        }
+            isAuthenticated: !isEmpty(action.payload),
+            user: action.payload,
+            userProfile: action.userProfile
+        };
         default:
             return state;
     }
-
 }
